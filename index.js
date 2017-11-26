79fa95012ec1717ac776029833badb7163ed1494
Jeff Walton <jwalton89@gmail.com>
2017-11-26 16:19:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,15 +2,12 @@ var a = 1275
 var b = 1
 function add(a,b) {return a+b}
 add()
-console.log()
 
 function subtract(a,b) {return a-b}
 subtract()
-console.log()
 
-function mulitply(a,b) {return a*b}
+function multiply(a,b) {return a*b}
 multiply()
-console.log()
 
 function divide(a,b) {return a/b}
 divide()
